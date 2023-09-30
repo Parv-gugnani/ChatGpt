da0c14e0a41db87f3940925b2a40e1b20e845121
Parv-gugnani <9765.parv@gmail.com>
2023-09-30 12:36:19 +0530

    normalize css

--- chatgpt/src/App.js
+++ chatgpt/src/App.js
@@ -7,21 +7,22 @@ function App() {
       <aside className="sidemenu">
         <h1>Aisde</h1>
       </aside>
-      <section></section>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="chatbox">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </section>
     </div>
   );
 }
